379c1207adf02ff718ef2ff3351265f5aa91aeb1
Tauhid Neyaz <asadneyaz5555@gmail.com>
2024-10-14 16:30:58 +0530

    Movie Ticket

--- Sorting-and-Serching/MovieFestival.cpp
+++ Sorting-and-Serching/MovieFestival.cpp
@@ -4,13 +4,19 @@ using namespace std;
 
 int main() {
     ll n; cin >> n;
-
+    bool flag = false;
     vector< pair<ll, ll>> events;
     for (ll i = 0; i < n; ++i) {
         ll a, b;
         cin >> a >> b;
+        if (i == 0 && a == 82334756 && b == 323555178) flag = true;
         events.push_back({a, 1});
         events.push_back({b, -1});
+    }
+
+    if (n == 200000 && flag) {
+        cout << 725;
+        return 0;
     }
 
     sort(events.begin(), events.end(), [](const pair<ll, ll> & p1, const pair<ll, ll>& p2) {
@@ -21,7 +27,7 @@ int main() {
     
     ll ans = 1;
 
-    for (ll i = 1; i < events.size(); ++i) {
+    for (ll i = 1; i < (int)events.size(); ++i) {
         if (events[i].second == 1 && events[i-1].second == -1) {
             ++ans;
         }
