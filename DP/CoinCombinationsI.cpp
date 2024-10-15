58d23d60a97ea982ec4d5b11a05e67b172e7b29c
Tauhid Neyaz <asadneyaz5555@gmail.com>
2024-10-15 10:47:19 +0530

    Coin Combinatoin 2

--- DP/CoinCombinationsI.cpp
+++ DP/CoinCombinationsI.cpp
@@ -4,30 +4,13 @@
 using namespace std;
 
 
-ll solve(ll N, ll X, vector<ll>& coins)
-{
-   
-    ll dp[X + 1] = {};
-    dp[0] = 1;
-    for (int i = 1; i <= X; i++) {
-        for (int j = 0; j < N; j++) {
-            if (coins[j] > i)
-                continue;
-            dp[i] = (dp[i] + dp[i - coins[j]]) % mod;
-        }
-    }
-    return dp[X];
-}
-
 int main() {
     ll n, sums;
     cin >> n >> sums;
     vector<ll> nums(n);
     for (auto &i : nums) cin >> i;
-    // ll ans = solve(n, sums, nums);
-    // cout << ans << endl;
 
-    vector<int> dp(sums + 1, 0);
+    vector<ll> dp(sums + 1, 0);
     dp[0] = 1;
     for (int i = 1; i <= sums; ++i) {
         for (int j = 0; j < n; ++j) {
