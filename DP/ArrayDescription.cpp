f89dc7e270dbbc18a5271fade3bb6bac2422f353
Tauhid Neyaz <asadneyaz5555@gmail.com>
2024-10-15 17:47:34 +0530

    Array Description

--- DP/ArrayDescription.cpp
+++ DP/ArrayDescription.cpp
@@ -3,32 +3,10 @@ using namespace std;
 
 #define ll long long
 
-int mod = 1e9 + 7;
-int dp[100005];
-
-int fn(vector<ll>& nums, ll idx, ll val, ll m) {
-
-    if (idx == nums.size()) return 1;
-    if (val < 1 && val > m) return 0;
-
-    if (nums[idx] != 0) {
-        if (nums[idx] == val) {
-            return fn(nums, idx + 1, val, m);
-        } else {
-            return 0;
-        }
-    }
-
-    ll ans = 0;
-
-    ans = fn(nums, idx + 1, val + 1, m) + 
-            fn(nums, idx + 1, val - 1, m) + 
-            fn(nums, idx + 1, val, m);
-    
-    return ans;
-            
-
+bool valid(ll x, ll m) {
+    return x > 0 && x <= m;
 }
+int mod = 1e9 + 7;
 
 int main() {
     ll n, m;
@@ -36,12 +14,38 @@ int main() {
     vector<ll> v(n);
     for (auto &i : v) cin >> i;
 
-    ll sum = 0;
-    for (int val = 1; val <= m; ++val) {
-        sum += (fn(v, 0, val, m) % mod);
+    vector< vector<ll>> dp(n + 1, vector<ll>(m + 1));
+    // dp[i][j] = numsber of way to build a prefix array of size i such that
+    // the last element of prefix is k
+
+    // base case
+    for (int i = 1; i <= m; ++i) {
+        if (v[0] == 0 || v[0] == i) {
+            dp[1][i] = 1;
+        }
     }
 
-    cout << sum << endl;
+    for (int i = 2; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
 
-    
+            if (v[i-1] != 0 && v[i-1] != j) {
+                dp[i][j] = 0;
+                continue;
+            }
+            // diif is atmost 1, so (j - 1, j, j + 1)
+            for (ll prev = j - 1; prev <= j + 1; ++prev) {
+                if (!valid(prev, m))
+                    continue;
+                
+                dp[i][j] = (dp[i][j] + (dp[i-1][prev]) % mod);
+            }
+        }
+    }
+
+    ll ans = 0;
+    for (int i = 1; i <= m; ++i) {
+        ans = (ans + dp[n][i]) % mod;
+    }
+
+    cout << ans << endl;
 }
