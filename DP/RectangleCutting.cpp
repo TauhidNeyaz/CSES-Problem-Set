45607f9943f0df0eb797865539460f2bf8521bf3
Tauhid Neyaz <asadneyaz5555@gmail.com>
2024-10-18 16:43:31 +0530

    Rectangle Cutting memo and table both

--- DP/RectangleCutting.cpp
+++ DP/RectangleCutting.cpp
@@ -1,11 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector< vector<int>> memo;
+
+int fn(int n, int m) {
+
+    if (n == m)
+        return 0;
+    
+    if (memo[n][m] != -1) return memo[n][m];
+
+    int res = 1e9;
+
+    for (int v = 1; v <= n - 1; ++v) {
+        res = min(res, 1 + fn(v, m) + fn(n - v, m));
+    }
+
+    for (int h = 1; h <= m - 1; ++h) {
+        res = min(res, 1 + fn(n, h) + fn(n, m - h));
+    }
+
+    return memo[n][m] = res;
+}
+
 int main() {
     int n, m;
     cin >> n >> m;
 
+    memo.resize(n + 1, vector<int>(m + 1, -1));
+    // cout << fn(n, m) << endl; gives TLE in 2 cases
+
     vector< vector<int>> dp(n + 1, vector<int>(m + 1, 1e9));
+    
 
     for (int i = 1; i <= n; ++i) {
         for (int j = 1; j <= m; ++j) {
