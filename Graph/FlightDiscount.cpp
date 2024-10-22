13692a402843324c3d533d80cc204e1590ae57ac
Tauhid Neyaz <asadneyaz5555@gmail.com>
2024-10-22 16:42:53 +0530

    Flight Discount

--- Graph/FlightDiscount.cpp
+++ Graph/FlightDiscount.cpp
@@ -1,96 +1,81 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <algorithm>
+#include <cmath>
+#include <limits>
 
 #define loop(i, l, r) for (int i = (int)(l); i <= (int)(r); ++i)
-#define rloop(i, l, r) for (int i = (int)(r); i >= (int)(l); --i)
-
-#define pb push_back
-#define fi first
-#define se second
-
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define sz(a) int((a).size())
 
 using namespace std;
 
 typedef long long ll;
-typedef vector<int> vi;
-typedef vector<vi> vvi;
-typedef pair<int, int> pp;
-typedef vector<pp> vpp;
-typedef vector<ll> vll;
-typedef vector<vll> vvll;
-typedef double ld;
 typedef pair<ll, ll> pll;
-typedef unordered_map<ll, vll> uG;
 typedef unordered_map<ll, vector<pll>> wG;
 
-const int inf = 1e9;
 const ll INF = 1e18;
-const int MOD = 1e9 + 7;
 
-vll dis, vis, path, cst;
-map<string, int> mpp;
+vector<ll> temp, dis, rev_dis, vis;
 
-void SP(wG &G) {
-    dis[1] = 0;
+void SP(wG &G, int node) {
+    dis.assign(dis.size(), INF);
+    vis.assign(vis.size(), 0);
+    dis[node] = 0;
     priority_queue<pll, vector<pll>, greater<pll>> pq;
-    pq.push({0, 1}); 
+    pq.push({0, node}); 
 
     while (!pq.empty()) {
         pll cur = pq.top();
         pq.pop();
 
-        if (vis[cur.se]) continue;
-        vis[cur.se] = 1;
+        if (vis[cur.second]) continue;
+        vis[cur.second] = 1;
 
-        for (auto &nbr : G[cur.se]) {
-            
-            if (dis[nbr.fi] > dis[cur.se] + nbr.se) {
-                path[nbr.fi] = cur.se; 
-                dis[nbr.fi] = dis[cur.se] + nbr.se; 
-                pq.push({dis[nbr.fi], nbr.fi}); 
+        for (auto &nbr : G[cur.second]) {
+            if (dis[nbr.first] > dis[cur.second] + nbr.second) {
+                dis[nbr.first] = dis[cur.second] + nbr.second;
+                pq.push({dis[nbr.first], nbr.first});
             }
         }
     }
 }
 
-void p_to_c(int node) {
-    if (path[node] == -1) {
-        return;
-    }
-    string s = to_string(path[node]) + to_string(node);
-    cst.pb(mpp[s]);
-    p_to_c(path[node]);
-}
-
 void Tauhid() {
-    
     int V, E;
     cin >> V >> E;
 
-    wG G;
+    wG G, revG;
     loop(i, 1, E) {
         int u, v, w;
         cin >> u >> v >> w;
-
-        G[u].pb({v, w});
-        string s = to_string(u) + to_string(v);
-        mpp[s] = w;
+        G[u].push_back({v, w});
+        revG[v].push_back({u, w});
     }
 
-    dis.resize(V + 1, INF);
-    vis.resize(V + 1, 0);
-    path.resize(V + 1, -1);
-    cst.clear(); 
+    dis.resize(V + 1);
+    rev_dis.resize(V + 1);
+    vis.resize(V + 1);
 
-    SP(G);
-    p_to_c(V);
-    ll sum = accumulate(all(cst), 0LL);
-    ll mxx = *max_element(all(cst));
-    ll ff = floor(mxx * 1.0 / 2.0);
+    SP(G, 1);
+    temp = dis;
 
-    cout << (sum - mxx) + ff;
+    SP(revG, V);
+    rev_dis = dis;
+
+    ll ans = INF;
+
+    for (auto &node : G) {
+        int nd = node.first;
+        vector<pll> eg = node.second;
+
+        for (auto &p : eg) {
+            ll div = p.second / 2;
+            ans = min(ans, temp[nd] + rev_dis[p.first] + div);
+        }
+    }
+
+    cout << ans << endl;
 }
 
 int main() {
